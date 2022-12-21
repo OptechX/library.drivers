383cee8276c61b8156a02777147a7b30c3751cfb
danijeljw-rpc <danijel@repasscloud.com>
2022-12-22 07:49:10 +1100

    verify yaml

---
+++ Dell/main.ps1
@@ -0,0 +1,10 @@
+# variables
+$c = $PSScriptRoot
+$dell_cab_file = "https://downloads.dell.com/catalog/DriverPackCatalog.cab"
+$cab_file = "${c}/$(Split-Path -Path $dell_cab_file -Leaf)"
+
+# get the dell driver CAB file
+Invoke-WebRequest -Uri $dell_cab_file -UseBasicParsing -DisableKeepAlive -OutFile $cab_file
+
+# expand the cab file locally
+cabextract $cab_file
