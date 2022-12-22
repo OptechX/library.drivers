cc343c267c8a6a01b9327e4e0b5d1ca19f23c7a0
danijeljw-rpc <danijel@repasscloud.com>
2022-12-23 10:23:52 +1030

    updates, add Lenovo script

--- Dell/test.ps1
+++ Dell/test.ps1
@@ -3,17 +3,25 @@ $c = $PSScriptRoot
 $catalogXMLFile = "${c}/DriverPackCatalog.xml"
 
 
-
+# read all xml data into variable x
 [xml]$x = Get-Content -Path $catalogXMLFile
 
+# assign intended data from x to packs
 $packs = $x.DriverPackManifest.DriverPackage
 
+# iterate of each pack
 foreach ($pack in $packs)
 {
-    #$pack
+    $pack
 }
 
-
+# read xml catalog
 [xml]$catalogXMLDoc = Get-Content $catalogXMLFile
 
-$catalogXMLDoc.DriverPackManifest.DriverPackage | Select-Object @{Expression={$_.SupportedSystems.Brand.key};Label="LOBKey";},  @{Expression={$_.SupportedSystems.Brand.prefix};Label="LOBPrefix";}, @{Expression={$_.SupportedSystems.Brand.Model.systemID};Label="SystemID";}, @{Expression={$_.SupportedSystems.Brand.Model.name};Label="SystemName";} –unique
+# print data to screen
+$catalogXMLDoc.DriverPackManifest.DriverPackage | 
+    Select-Object `
+        @{Expression={$_.SupportedSystems.Brand.key};Label="LOBKey";},  `
+        @{Expression={$_.SupportedSystems.Brand.prefix};Label="LOBPrefix";}, `
+        @{Expression={$_.SupportedSystems.Brand.Model.systemID};Label="SystemID";}, `
+        @{Expression={$_.SupportedSystems.Brand.Model.name};Label="SystemName";} -Unique
--- Lenovo/main.ps1
+++ Lenovo/main.ps1
@@ -1,1 +1,17 @@
-$catalog = "https://download.lenovo.com/cdrt/td/catalogv2.xml"
+# variables
+$c = $PSScriptRoot
+$lenovo_catalog_xml = "https://download.lenovo.com/cdrt/td/catalogv2.xml"
+$xml_file = "${c}/$(Split-Path -Path $lenovo_catalog_xml -Leaf)"
+$catalogXMLFile = "${c}/${xml_file}"
+
+# get the Lenovo xml file
+Invoke-WebRequest -Uri $lenovo_catalog_xml -UseBasicParsing -DisableKeepAlive -OutFile $xml_file
+
+
+# show me all the files
+tree
+
+# read the exported XML file to variable
+[xml]$catalogXMLDoc = Get-Content $catalogXMLFile
+
+$catalogXMLDoc
