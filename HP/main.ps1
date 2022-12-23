883c1848e6758eb1b17bab3361bb21f40739f202
danijeljw-rpc <danijel@repasscloud.com>
2022-12-23 11:50:25 +1030

    updates, add Lenovo script

---
+++ HP/main.ps1
@@ -0,0 +1,13 @@
+# variables
+$c = $PSScriptRoot
+$dell_cab_file = "https://hpia.hpcloud.hp.com/downloads/sccmcatalog/HpCatalogForSms.latest.cab"
+$cab_file = "${c}/data/$(Split-Path -Path $dell_cab_file -Leaf)"
+
+# get the dell driver CAB file
+Invoke-WebRequest -Uri $dell_cab_file -UseBasicParsing -DisableKeepAlive -OutFile $cab_file
+
+# expand the cab file locally
+cabextract $cab_file --directory "HP/data"
+
+# show me all the files
+tree
