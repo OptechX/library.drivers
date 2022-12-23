b4029197ec75b22e9c8d44ec68f802b395b11e50
danijeljw-rpc <danijel@repasscloud.com>
2022-12-23 10:58:20 +1030

    updates, add Lenovo script

--- Lenovo/main.ps1
+++ Lenovo/main.ps1
@@ -3,9 +3,7 @@ $c = $PSScriptRoot
 $lenovo_catalog_xml = "https://download.lenovo.com/cdrt/td/catalogv2.xml"
 $xml_file = "${c}/$(Split-Path -Path $lenovo_catalog_xml -Leaf)"
 $catalogXMLFile = "${c}${xml_file}"
-Write-Output "Path is: $catalogXMLFile"
-Test-Path -Path ./Lenovo/catalogv2.xml
-"Is it true above?"
+
 
 # get the Lenovo xml file
 Invoke-WebRequest -Uri $lenovo_catalog_xml -UseBasicParsing -DisableKeepAlive -OutFile $xml_file
@@ -18,3 +16,11 @@ tree
 #[xml]$catalogXMLDoc = Get-Content $catalogXMLFile
 
 #$catalogXMLDoc
+
+
+
+Write-Output "Path is: $catalogXMLFile"
+Test-Path -Path ./Lenovo/catalogv2.xml
+"Is it true above?"
+Test-Path -Path $catalogXMLFile
+"Is it true above?"
