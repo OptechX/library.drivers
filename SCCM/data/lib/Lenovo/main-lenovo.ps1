c275525c1980a8ae03354d5225b674a932309712
danijeljw-rpc <danijel@repasscloud.com>
2022-12-25 17:24:34 +1030

    lenovo gh-action test

--- SCCM/data/lib/Lenovo/main-lenovo.ps1
+++ SCCM/data/lib/Lenovo/main-lenovo.ps1
@@ -14,88 +14,88 @@ $XmlDocument.SystemsManagementCatalog.SoftwareDistributionPackage
 # create blank array
 $pclist = @()
 
-
 # iterate through objects and add to the array
 foreach ($json in $json_list)
 {
-    $pclist += Get-Content -Path $json | ConvertFrom-Json | Select-Object -ExpandProperty DisplayName
+    $pclist += Get-Content -Path $json | `
+        ConvertFrom-Json | `
+        Select-Object -ExpandProperty DisplayName
 }
 
-$pclist
+# resort list
+$sorted_pclist = $pclist | Sort-Object
+[System.String[]]$ignore_list = Get-Content -Path "${c}/lenovo_ignore_list.txt"
+$lenovo_pc_list = $sorted_pclist | Where-Object -FilterScript {$_ -notin $ignore_list}
 
-# # resort list
-# $sorted_pclist = $pclist | Sort-Object
-# [System.String[]]$ignore_list = Get-Content -Path "${c}/dell_ignore_list.txt"
-# $dell_pc_list = $sorted_pclist | Where-Object -FilterScript {$_ -notin $ignore_list}
+# show list
+foreach ($pc in $pc_list)
+{
+    switch($pc)
+    {
+        {$_ -match '^ThinkCentre\sThinkCentre\s'}
+        {
+            $uid = "Lenovo::ThinkCentre::" + $pc.Replace('ThinkCentre ThinkCentre ','').Replace(' ','_')
+            $make = "ThinkCentre"
+            $model = $pc.Replace('ThinkCentre ThinkCentre ','')
+        }
 
-# # show list
-# foreach ($dell_pc in $dell_pc_list)
-# {
-#     switch($dell_pc)
-#     {
-#         # {$_ -match '^Internet\sof\sThings\s'}
-#         # {
-#         #     $uid = "Dell::IoT::" + $dell_pc.Replace('Internet of Things ','').Replace(' ','_')
-#         #     $make = "IoT"
-#         #     $model = $dell_pc.Replace('Internet of Things ','')
-#         # }
-#         {$_ -match '^Latitude\s'}
-#         {
-#             $uid = "Dell::Latitude::" + $dell_pc.Replace('Latitude ','').Replace(' ','_')
-#             $make = "Latitude"
-#             $model = $dell_pc.Replace('Latitude ','')
-#         }
+        {$_ -match '^ThinkCentre\s'}
+        {
+            $uid = "Lenovo::ThinkCentre::" + $pc.Replace('ThinkCentre ','').Replace(' ','_')
+            $make = "ThinkCentre"
+            $model = $pc.Replace('ThinkCentre ','')
+        }
 
-#         {$_ -match '^OptiPlex\s'}
-#         {
-#             $uid = "Dell::OptiPlex::" + $dell_pc.Replace('OptiPlex ','').Replace(' ','_')
-#             $make = "OptiPlex"
-#             $model = $dell_pc.Replace('OptiPlex ','')
-#         }
+        {$_ -match '^ThinkPad\sX1\s'}
+        {
+            $uid = "Lenovo::ThinkPadX1::" + $pc.Replace('ThinkPad X1 ','').Replace(' ','_')
+            $make = "ThinkPad X1"
+            $model = $pc.Replace('ThinkPad X1 ','')
+        }
 
-#         {$_ -match '^Precision\s'}
-#         {
-#             $uid = "Dell::Precision::" + $dell_pc.Replace('Precision ','').Replace(' ','_')
-#             $make = "Precision"
-#             $model = $dell_pc.Replace('Precision ','')
-#         }
+        {$_ -match '^ThinkPad\sYoga\s'}
+        {
+            $uid = "Lenovo::ThinkPadYoga::" + $pc.Replace('ThinkPad Yoga ','').Replace(' ','_')
+            $make = "ThinkPad Yoga"
+            $model = $pc.Replace('ThinkPad Yoga ','')
+        }
 
-#         # {$_ -match '^Tablet\s'}
-#         # {
-#         #     $uid = "Dell::Tablet::" + $dell_pc.Replace('Tablet ','').Replace(' ','_')
-#         #     $make = "Tablet"
-#         #     $model = $dell_pc.Replace('Tablet ','')
-#         # }
+        {$_ -match '^ThinkPad\s'}
+        {
+            $uid = "Lenovo::ThinkPad::" + $pc.Replace('ThinkPad ','').Replace(' ','_')
+            $make = "ThinkPad"
+            $model = $pc.Replace('ThinkPad ','')
+        }
 
-#         {$_ -match '^XPS Notebook\s'}
-#         {
-#             $uid = "Dell::XPSNotebook::" + $dell_pc.Replace('XPS Notebook ','').Replace(' ','_')
-#             $make = "XPS Notebook"
-#             $model = $dell_pc.Replace('XPS Notebook ','')
-#         }
-#     }
+        {$_ -match '^ThinkStation\s'}
+        {
+            $uid = "Lenovo::ThinkStation::" + $pc.Replace('ThinkStation ','').Replace(' ','_')
+            $make = "ThinkStation"
+            $model = $pc.Replace('ThinkStation ','')
+        }
+    }
 
-#     $payload = @{
-#         id = 0
-#         uuid = [System.Guid]::NewGuid().ToString()
-#         uid = $uid
-#         originalEquipmentManufacturer = "Dell"
-#         make = $make
-#         model = $model
-#         productionYear = 2022
-#         cpuArch = @("x64")
-#         windowsOS = @("Windows 10","Windows 11")
-#     }
+    $payload = @{
+        id = 0
+        uuid = [System.Guid]::NewGuid().ToString()
+        uid = $uid
+        originalEquipmentManufacturer = "Lenovo"
+        make = $make
+        model = $model
+        productionYear = 2022
+        cpuArch = @("x64")
+        windowsOS = @("Windows 10","Windows 11")
+    }
 
-#     $json = $payload | ConvertTo-Json
-#     try
-#     {
-#         Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore" -Method Post -UseBasicParsing -Body $json -ContentType "application/json" -ErrorAction Stop
-#         Start-Sleep -Milliseconds 50
-#     }
-#     catch
-#     {
-#         Write-Error "Error: $($_.Exception)"
-#         $Body
-#     }
-# }
+    $json = $payload | ConvertTo-Json
+    try
+    {
+        Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore" -Method Post -UseBasicParsing -Body $json -ContentType "application/json" -ErrorAction Stop
+        Start-Sleep -Milliseconds 50
+    }
+    catch
+    {
+        Write-Error "Error: $($_.Exception)"
+        $Body
+    }
+}
