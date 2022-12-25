793c964ee8351cd112acba64c26ba917cb99abc5
danijeljw-rpc <danijel@repasscloud.com>
2022-12-25 12:55:48 +1030

    update gh-actions

--- tasks/clear-api.ps1
+++ tasks/clear-api.ps1
@@ -1,5 +1,6 @@
 foreach ($id in (Invoke-RestMethod -Uri https://engine.api.dev.optechx-data.com/v1/DriversCore -Method Get).id)
 {
-    Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore${id}" -Method Delete -Headers @{accept="json/application"}
+    Write-Output "Clearing record: ${id}"
+    Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore/${id}" -Method Delete -Headers @{accept="json/application"}
     Start-Sleep -Milliseconds 50
 }
