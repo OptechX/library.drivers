ae42450381b8b1f2aa74ab32f94323915f06fb89
danijeljw-rpc <danijel@repasscloud.com>
2022-12-26 16:38:29 +1030

    stash

--- SCCM/data/lib/Lenovo/main-lenovo.ps1
+++ SCCM/data/lib/Lenovo/main-lenovo.ps1
@@ -30,6 +30,8 @@ $lenovo_pc_list = $sorted_pclist | Where-Object -FilterScript {$_ -notin $ignore
 # show list
 foreach ($pc in $lenovo_pc_list)
 {
+    $valid = $false
+
     switch($pc)
     {
         {$_ -match '^ThinkCentre\sThinkCentre\s'}
@@ -37,6 +39,7 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkCentre::" + $pc.Replace('ThinkCentre ThinkCentre ','').Replace(' ','_')
             $make = "ThinkCentre"
             $model = $pc.Replace('ThinkCentre ThinkCentre ','')
+            $valid = $true
         }
 
         {$_ -match '^ThinkCentre\s'}
@@ -44,6 +47,7 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkCentre::" + $pc.Replace('ThinkCentre ','').Replace(' ','_')
             $make = "ThinkCentre"
             $model = $pc.Replace('ThinkCentre ','')
+            $valid = $true
         }
 
         {$_ -match '^ThinkPad\sX1\s'}
@@ -51,6 +55,7 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkPadX1::" + $pc.Replace('ThinkPad X1 ','').Replace(' ','_')
             $make = "ThinkPad X1"
             $model = $pc.Replace('ThinkPad X1 ','')
+            $valid = $true
         }
 
         {$_ -match '^ThinkPad\sYoga\s'}
@@ -58,6 +63,7 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkPadYoga::" + $pc.Replace('ThinkPad Yoga ','').Replace(' ','_')
             $make = "ThinkPad Yoga"
             $model = $pc.Replace('ThinkPad Yoga ','')
+            $valid = $true
         }
 
         {$_ -match '^ThinkPad\s'}
@@ -65,6 +71,7 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkPad::" + $pc.Replace('ThinkPad ','').Replace(' ','_')
             $make = "ThinkPad"
             $model = $pc.Replace('ThinkPad ','')
+            $valid = $true
         }
 
         {$_ -match '^ThinkStation\s'}
@@ -72,33 +79,41 @@ foreach ($pc in $lenovo_pc_list)
             $uid = "Lenovo::ThinkStation::" + $pc.Replace('ThinkStation ','').Replace(' ','_')
             $make = "ThinkStation"
             $model = $pc.Replace('ThinkStation ','')
+            $valid = $true
         }
     }
 
-    $payload = @{
-        id = 0
-        uuid = [System.Guid]::NewGuid().ToString()
-        uid = $uid
-        originalEquipmentManufacturer = "Lenovo"
-        make = $make
-        model = $model
-        productionYear = 2022
-        cpuArch = @("x64")
-        windowsOS = @("Windows 10","Windows 11")
-    }
-
-    $json = $payload | ConvertTo-Json
-
-    $json
-    
-    try
+    if ($valid)
     {
-        Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore" -Method Post -UseBasicParsing -Body $json -ContentType "application/json" -ErrorAction Stop
-        Start-Sleep -Milliseconds 50
+        $payload = @{
+            id = 0
+            uuid = [System.Guid]::NewGuid().ToString()
+            uid = $uid
+            originalEquipmentManufacturer = "Lenovo"
+            make = $make
+            model = $model
+            productionYear = 2022
+            cpuArch = @("x64")
+            windowsOS = @("Windows 10","Windows 11")
+        }
+
+        $json = $payload | ConvertTo-Json
+
+        $json
+        
+        try
+        {
+            Invoke-RestMethod -Uri "https://engine.api.dev.optechx-data.com/v1/DriversCore" -Method Post -UseBasicParsing -Body $json -ContentType "application/json" -ErrorAction Stop
+            Start-Sleep -Milliseconds 50
+        }
+        catch
+        {
+            Write-Error "Error: $($_.Exception)"
+            $Body
+        }
     }
-    catch
+    else
     {
-        Write-Error "Error: $($_.Exception)"
-        $Body
+        $pc
     }
 }
