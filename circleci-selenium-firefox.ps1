83ddd4a63f4aec086c25479023dbaa5c31aad7a3
danijeljw-rpc <danijel@repasscloud.com>
2022-12-28 10:48:54 +1030

    using firefox on circle-ci

--- circleci-selenium-firefox.ps1
+++ circleci-selenium-firefox.ps1
@@ -1,5 +1,13 @@
-Write-Output "Downloads dir: ${HOME}/Downloads"
-Test-Path -Path "${HOME}/Downloads"
+# variables
+$downloads_dir = "${HOME}/Downloads"
 
-Write-Output "Is 7zip installed?"
-7z
+# setup selenium
+$url = 'https://www.nuget.org/api/v2/package/Selenium.WebDriver/3.141.0'
+$d = ([System.Net.WebRequest]::CreateDefault($url)).GetResponse()
+$g = $d.ResponseUri.OriginalString
+$c = Split-Path -Path $d.ResponseUri.OriginalString -Leaf
+
+Invoke-WebRequest -Uri $g -OutFile "${downloads_dir}\${c}" -UseBasicParsing -DisableKeepAlive
+
+mkdir -p C:\Selenium
+7z x $HOME/Downloads/$c -oC:\Selenium
