6d12d569d91de683781c19303432c334c9bbc6a0
danijeljw-rpc <danijel@repasscloud.com>
2022-12-28 10:57:22 +1030

    circle-ci testing

--- circleci-selenium-firefox.ps1
+++ circleci-selenium-firefox.ps1
@@ -9,49 +9,51 @@ $c = Split-Path -Path $d.ResponseUri.OriginalString -Leaf
 
 Invoke-WebRequest -Uri $g -OutFile "${downloads_dir}\${c}" -UseBasicParsing -DisableKeepAlive
 
-mkdir -p C:\Selenium
-7z x "${downloads_dir}\${c}" -oC:\Selenium
+dir $downloads_dir
 
-# download firefox driver
-$url = 'https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-win32.zip'
-Invoke-WebRequest -Uri $url -OutFile "${downloads_dir}\geckodriver-v0.32.0-win32.zip"
-7z x "${downloads_dir}\geckodriver-v0.32.0-win32.zip" -oC:\Selenium
-Move-Item -Path C:\Selenium\geckodriver.exe -Destination C:\Selenium\lib\net45\geckodriver.exe
+# mkdir -p C:\Selenium
+# 7z x "${downloads_dir}\${c}" -oC:\Selenium
 
-# initialise Selenium driver
-$PathToFolder = 'C:\Selenium\lib\net45'
-[System.Reflection.Assembly]::LoadFrom("{0}\WebDriver.dll" -f $PathToFolder)
-if ($env:Path -notcontains ";$PathToFolder" ) {
-    $env:Path += ";$PathToFolder"
-}
+# # download firefox driver
+# $url = 'https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-win32.zip'
+# Invoke-WebRequest -Uri $url -OutFile "${downloads_dir}\geckodriver-v0.32.0-win32.zip"
+# 7z x "${downloads_dir}\geckodriver-v0.32.0-win32.zip" -oC:\Selenium
+# Move-Item -Path C:\Selenium\geckodriver.exe -Destination C:\Selenium\lib\net45\geckodriver.exe
 
-# function into memory
-Function Start-FirefoxBrowser{
-    Param(
-        $SiteURL
-    )
-    # Create Firefox options object
-    $firefoxoptions = new-object OpenQA.Selenium.Firefox.FirefoxOptions
-    # $firefoxoptions.Profile = "$workingPath\FireFoxProfile"
-    $firefoxoptions.BrowserExecutableLocation = "C:\Program Files\Mozilla Firefox\firefox.exe"
+# # initialise Selenium driver
+# $PathToFolder = 'C:\Selenium\lib\net45'
+# [System.Reflection.Assembly]::LoadFrom("{0}\WebDriver.dll" -f $PathToFolder)
+# if ($env:Path -notcontains ";$PathToFolder" ) {
+#     $env:Path += ";$PathToFolder"
+# }
+
+# # function into memory
+# Function Start-FirefoxBrowser{
+#     Param(
+#         $SiteURL
+#     )
+#     # Create Firefox options object
+#     $firefoxoptions = new-object OpenQA.Selenium.Firefox.FirefoxOptions
+#     # $firefoxoptions.Profile = "$workingPath\FireFoxProfile"
+#     $firefoxoptions.BrowserExecutableLocation = "C:\Program Files\Mozilla Firefox\firefox.exe"
  
-    # Start Firefox and navigate to URL
-    $firefoxdriver = New-Object OpenQA.Selenium.Firefox.FirefoxDriver($firefoxoptions)
-    $FirefoxDriver.Navigate().GoToURL($siteURL)
-    Return $FirefoxDriver
-}
+#     # Start Firefox and navigate to URL
+#     $firefoxdriver = New-Object OpenQA.Selenium.Firefox.FirefoxDriver($firefoxoptions)
+#     $FirefoxDriver.Navigate().GoToURL($siteURL)
+#     Return $FirefoxDriver
+# }
 
 
-$objFirefoxDriver = Start-FirefoxBrowser -SiteURL 'https://www.microsoft.com/download/details.aspx?id=101315'
+# $objFirefoxDriver = Start-FirefoxBrowser -SiteURL 'https://www.microsoft.com/download/details.aspx?id=101315'
 
 
-$objFirefoxDriver.FindElementByXPath('/html/body/main/div/div/form/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/div/div/div/a').Click()
-$objFirefoxDriver.FindElementByXPath('/html/body/main/div/div/form/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div/div[2]/div[1]/div[1]/div/div[1]/input').Click()
-$objFirefoxDriver.FindElementByXPath('//*[@id="5b70c241-07ba-40b9-c0c1-6aae74ab472b"]').Click()
+# $objFirefoxDriver.FindElementByXPath('/html/body/main/div/div/form/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/div/div/div/a').Click()
+# $objFirefoxDriver.FindElementByXPath('/html/body/main/div/div/form/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div/div[2]/div[1]/div[1]/div/div[1]/input').Click()
+# $objFirefoxDriver.FindElementByXPath('//*[@id="5b70c241-07ba-40b9-c0c1-6aae74ab472b"]').Click()
 
-1..30 | ForEach-Object {
-    $i = $_
-    Write-Output "Check number: ${i}"
-    Get-ChildItem -Path $downloads_dir
-    Start-Sleep -Seconds 3
-}
+# 1..30 | ForEach-Object {
+#     $i = $_
+#     Write-Output "Check number: ${i}"
+#     Get-ChildItem -Path $downloads_dir
+#     Start-Sleep -Seconds 3
+# }
