31f320c4ee72b87473a1fd83a905145d087ef5da
danijeljw-rpc <danijel@repasscloud.com>
2022-12-29 14:06:43 +1030

    python tests

--- tests/download-check.ps1
+++ tests/download-check.ps1
@@ -7,33 +7,33 @@ foreach ($file in $files)
     {
         if ($file -like "*_Win10_*")
         {
-            Write-Output "Surface Book is Windows 10"
+            Write-Host "Surface Book is Windows 10"
         }
         if ($file -like "*_Win11_*")
         {
-            Write-Output "Surface Book is Windows 11"
+            Write-Host "Surface Book is Windows 11"
         }
     }
     if ($file -like "SurfaceBook2_Win*")
     {
         if ($file -like "*_Win10_*")
         {
-            Write-Output "Surface Book 2 is Windows 10"
+            Write-Host "Surface Book 2 is Windows 10"
         }
         if ($file -like "*_Win11_*")
         {
-            Write-Output "Surface Book 2 is Windows 11"
+            Write-Host "Surface Book 2 is Windows 11"
         }
     }
     if ($file -like "SurfaceBook3_Win*")
     {
         if ($file -like "*_Win10_*")
         {
-            Write-Output "Surface Book 3 is Windows 10"
+            Write-Host "Surface Book 3 is Windows 10"
         }
         if ($file -like "*_Win11_*")
         {
-            Write-Output "Surface Book 3 is Windows 11"
+            Write-Host "Surface Book 3 is Windows 11"
         }
     }
 }
