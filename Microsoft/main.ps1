3d3d2f7350e823857e75611a730dbbf94961f003
danijeljw-rpc <danijel@repasscloud.com>
2022-12-29 11:34:20 +1030

    update files

--- Microsoft/main.ps1
+++ Microsoft/main.ps1
@@ -2,7 +2,7 @@
 # Surface Book 3
 Import-Module -Name Selenium
 $Driver = Start-SeChrome
-Start-Sleep -Seconds 46
+Start-Sleep -Seconds 10
 Enter-SeUrl https://www.microsoft.com/download/details.aspx?id=101315 -Driver $Driver
 $Element = Find-SeElement -Driver $Driver -Selection '/html/body/main/div/div/form/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/div/div/div/a'
 Invoke-SeClick -Element $Element
@@ -12,8 +12,8 @@ $Element = FInd-SeElement -Driver $Driver -Selection '//*[@id="5b70c241-07ba-40b
 Invoke-SeClick -Element $Element
 $a = 0
 do {
-    $b = (Get-ChildItem -Path ~\Downloads -Filter "*.part").Count
-    Start-Sleep -Seocnds 30
+    $b = (Get-ChildItem -Path $HOME\Downloads -Filter "*.part").Count
+    Start-Sleep -Seconds 30
 } until (
     $b -eq $a
 )
