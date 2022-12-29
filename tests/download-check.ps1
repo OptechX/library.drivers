6856b0e9863cc1937e75d7728527e5b76e602cac
danijeljw-rpc <danijel@repasscloud.com>
2022-12-29 14:01:56 +1030

    python tests

--- tests/download-check.ps1
+++ tests/download-check.ps1
@@ -1,3 +1,39 @@
-$PWD
+$files = Get-ChildItem -Path /home/runner/work/optechx.drivers/optechx.drivers/downloads -Filter "*.msi"
 
-Get-ChildItem -Path /home/runner/work/optechx.drivers/optechx.drivers/downloads
+
+foreach ($file in $files)
+{
+    if ($file -like "SurfaceBook_Win*")
+    {
+        if ($file -like "*_Win10_*")
+        {
+            Write-Out "Surface Book is Windows 10"
+        }
+        if ($file -like "*_Win11_*")
+        {
+            Write-Out "Surface Book is Windows 11"
+        }
+    }
+    if ($file -like "SurfaceBook2_Win*")
+    {
+        if ($file -like "*_Win10_*")
+        {
+            Write-Out "Surface Book 2 is Windows 10"
+        }
+        if ($file -like "*_Win11_*")
+        {
+            Write-Out "Surface Book 2 is Windows 11"
+        }
+    }
+    if ($file -like "SurfaceBook3_Win*")
+    {
+        if ($file -like "*_Win10_*")
+        {
+            Write-Out "Surface Book 3 is Windows 10"
+        }
+        if ($file -like "*_Win11_*")
+        {
+            Write-Out "Surface Book 3 is Windows 11"
+        }
+    }
+}
