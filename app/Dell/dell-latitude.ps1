6d02fb357179dbfc3455e780c32b37dead79a593
danijeljw-rpc <danijel@repasscloud.com>
2023-01-30 21:59:49 +1100

    update logic

--- app/Dell/dell-latitude.ps1
+++ app/Dell/dell-latitude.ps1
@@ -41,7 +41,100 @@ foreach ($csv in $CsvFiles)
         $payload.windowsOS = @($windowsOS)
 
         # TRIGGER UPDATE
-        Update-ApiDriversCore -Payload $payload
+        # Update-ApiDriversCore -Payload $payload
+
+        [System.String]$API_BASE_URI = 'https://engine.api.prod.optechx-data.com'
+        $UID = $Payload.uid
+        try {
+            $API_RESPONSE = Invoke-WebRequest -Uri "${API_BASE_URI}/v1/DriversCore/uid/${UID}" -Method Get -UseBasicParsing -SkipHttpErrorCheck -ErrorAction Stop
+    
+            switch ($API_RESPONSE.StatusCode)
+            {
+                # 404 == object not found
+                404 {
+                    try {
+                        $json = $Payload | ConvertTo-Json
+                        Invoke-RestMethod -Uri "${API_BASE_URI}/v1/DriversCore" -Method Post -UseBasicParsing -Body $json -ContentType "application/json"
+                    }
+                    catch {
+                        Write-Output "MAIN LOGIC 404 ERROR"
+                        Write-Error $_.Exception
+                        Write-Output "START DIAGNOSTIC DATA>>>"
+                        Write-Output $json
+                        Write-Output "<<<END DIAGNOSTIC DATA"
+                    }
+                }
+                # 200 == object found
+                200 {
+                    Write-Output "200 == object found"
+                    [DriversCorePayload]$MATCHED_DATA = $API_RESPONSE.Content | ConvertFrom-Json
+    
+                    [System.Boolean]$INVOKE_UPDATE = $false
+    
+                    # update CpuArch (if required)
+                    [System.Collections.ArrayList]$tmpArrayList = $Payload.cpuArch
+                    if ($MATCHED_DATA.cpuArch -notin $tmpArrayList)
+                    {
+                        Write-Output "cpuArch not matched"
+                        $tmpArrayList.Add($MATCHED_DATA.cpuArch)
+                        $Payload.cpuArch = $tmpArrayList.ToArray()
+                        $INVOKE_UPDATE = $true
+                    }
+    
+                    # update WindowsOS (if required)
+                    [System.Collections.ArrayList]$tmpArrayList = $Payload.windowsOS
+                    if ($MATCHED_DATA.windowsOS -notin $tmpArrayList)
+                    {
+                        Write-Output "windowsOS not matched"
+                        $tmpArrayList.Add($MATCHED_DATA.windowsOS)
+                        $Payload.windowsOS = $tmpArrayList.ToArray()
+                        $INVOKE_UPDATE = $true
+                    }
+    
+                    # update entry date
+                    if ([int]$Payload.productionYear -gt [int]$MATCHED_DATA.productionYear)
+                    {
+                        Write-Output "productionYear not matched"
+                        $INVOKE_UPDATE = $true
+                    }
+    
+                    if ($INVOKE_UPDATE)
+                    {
+                        # retain existing values required
+                        $Payload.id = $MATCHED_DATA.id
+                        $Payload.uuid = $MATCHED_DATA.uuid
+    
+                        # create JSON object
+                        $json = $Payload | ConvertTo-Json
+
+                        Write-Output "Object to update:"
+                        $json
+    
+                        # update API endpoint with new data
+                        try {
+                            Write-Output "Update URI: ${API_BASE_URI}/v1/DriversCore/$($PAYLOAD.id)"
+                            Invoke-RestMethod -Uri "${API_BASE_URI}/v1/DriversCore/$($PAYLOAD.id)" -Method Put -UseBasicParsing -Body $json -Headers @{"Content-Type"="application/json"} -ErrorAction Stop
+                        } catch {
+                            Write-Output "match in matched_data error 2"
+                            "${API_BASE_URI}/v1/DriversCore/$($json.id)"
+                            $json
+                        }
+                    }
+                }
+                Default {
+                    $resp_code = $API_RESPONSE.StatusCode
+                    Write-Output "Unknown error: ${resp_code}"
+                }
+            }
+        }
+        catch {
+            <#Do this if a terminating exception happens#>
+            Write-Output "TERMINATING RESPONSE RECEIVED"
+            Write-Error $_.Exception
+            Write-Output "START DIAGNOSTIC DATA>>>"
+            Write-Output $UID
+            Write-Output "<<<END DIAGNOSTIC DATA"
+        }
     }
 }
 #endregion
