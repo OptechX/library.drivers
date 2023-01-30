17560876cce18c1df9330ebe357fb4b36db21219
danijeljw-rpc <danijel@repasscloud.com>
2023-01-30 12:13:02 +1100

    update workflows

---
+++ src/Dell/OptiPlex/win11-dell-optiplex.py
@@ -0,0 +1,117 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import fileinput
+import sys
+
+print(f'Running script -> {sys.argv[0]}')
+
+# Dell Optiplex
+url = 'https://www.dell.com/support/kbdoc/en-au/000109785/dell-command-deploy-driver-packs-for-optiplex-models'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+data_table = soup.find('table', class_ = 'table-bordered table-hover table-condensed')
+
+# define Windows 10 Optiplex CSV
+win11_optiplex_csv = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output/win11-dell-optiplex.csv')
+win11_optiplex_csv2 = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output/win11-dell-optiplex2.csv')
+
+# remove if file exists
+try:
+    os.remove(win11_optiplex_csv)
+    os.remove(win11_optiplex_csv2)
+except OSError:
+    pass
+
+# create header row on CSV
+header_row = "Make,Model,Updated"
+with open(win11_optiplex_csv, 'a') as f:
+            f.write(header_row)
+            f.write('\n')
+
+# extract data
+for machine in data_table.find_all('tbody'):
+    rows = machine.find_all('tr')
+    for row in rows:
+        # set count of columns for row
+        pl_count = row.find_all('td')
+
+        # 5 items means it's Win10 or Win11 ONLY
+        if (len(pl_count) == 5):
+            # get the text of cell 2, index 1, if it is "Not Applicable", it's a Win11 only machine
+            pl_check = row.find_all('td')[1].text.strip()
+            if (pl_check == 'Not Applicable'):
+                pl_date = row.find_all('td')[4].text.strip()
+                # get the machine name
+                pl_machine = row.find('td').text\
+                    .replace('\n','')\
+                    .replace('        ','')\
+                    .replace(u"\u00A0"," ")\
+                    .replace('Optiplex ','Optiplex,')\
+                    .replace('0(','0 (')\
+                    .replace('1(','1 (')\
+                    .replace('2(','2 (')\
+                    .replace('3(','3 (')\
+                    .replace('4(','4 (')\
+                    .replace('5(','5 (')\
+                    .replace('6(','6 (')\
+                    .replace('7(','7 (')\
+                    .replace('8(','8 (')\
+                    .replace('9(','9 (')\
+                    .strip()
+                pl_machine = re.sub('2.*in.*1','2-in-1', pl_machine)
+                # print(f'{pl_machine} --> {pl_date}')
+                output = pl_machine + "," + pl_date
+                with open(win11_optiplex_csv, 'a') as f:
+                    f.write(output)
+                    f.write('\n')
+        if (len(pl_count) == 7):
+            # is a win11 and win11 machine
+            pl_date = row.find_all('td')[6].text.strip()
+            # get the machine name
+            pl_machine = row.find('td').text\
+                .replace('\n','')\
+                .replace('        ','')\
+                .replace(u"\u00A0"," ")\
+                .replace('Optiplex ','OptiPlex,')\
+                .replace('0(','0 (')\
+                .replace('1(','1 (')\
+                .replace('2(','2 (')\
+                .replace('3(','3 (')\
+                .replace('4(','4 (')\
+                .replace('5(','5 (')\
+                .replace('6(','6 (')\
+                .replace('7(','7 (')\
+                .replace('8(','8 (')\
+                .replace('9(','9 (')\
+                .strip()
+            pl_machine = re.sub('2.*in.*1','2-in-1', pl_machine)
+            # print(f'{pl_machine} --> {pl_date}')
+            output = pl_machine + "," + pl_date
+            with open(win11_optiplex_csv, 'a') as f:
+                f.write(output)
+                f.write('\n')
+
+
+
+# remove the broken line
+bad_lines = ['3090 Ultra,01/24/2023']
+with open(win11_optiplex_csv) as oldfile, open(win11_optiplex_csv2, 'w') as newfile:
+    for line in oldfile:
+        if not any(bad_line in line for bad_line in bad_lines):
+            newfile.write(line)
+
+
+with fileinput.FileInput(win11_optiplex_csv2, inplace=True) as file:
+    for line in file:
+        print(line.replace('Optiplex', 'OptiPlex'), end='')
+
+
+
+# swap files around
+try:
+    os.remove(win11_optiplex_csv)
+    os.rename(win11_optiplex_csv2, win11_optiplex_csv)
+except OSError:
+    pass
---
+++ src/Lenovo/ThinkCentre/lenovo-thinkcentre.py
@@ -0,0 +1,104 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
+import sys
+
+print(f'Running script -> {sys.argv[0]}')
+
+# html file variables
+html_src = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output/page_source.html')
+output_csv = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output/lenovo-thinkcentre.csv')
+tmp_txt = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output/temp.txt')
+csv_header = 'Model,Win7,Win8.1,Win10,Win11'
+
+# remove if file exists
+try:
+    os.remove(html_src)
+    os.remove(output_csv)
+    os.remove(tmp_txt)
+except OSError:
+    pass
+
+# create blank csv
+with open(output_csv, 'a') as f:
+    f.write(csv_header)
+    f.write('\n')
+
+options = webdriver.ChromeOptions() 
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+# driver.maximize_window()
+
+driver.get("https://support.lenovo.com/au/en/solutions/ht074984")
+pageSource = driver.page_source
+fileToWrite = open(html_src, "w")
+fileToWrite.write(pageSource)
+fileToWrite.close()
+fileToRead = open(html_src, "r")
+fileToRead.close()
+driver.quit()
+
+# now to use bs4
+with open(html_src, 'r') as f:
+    contents = f.read()
+
+    soup = BeautifulSoup(contents, 'lxml')
+    data_table = soup.find_all('table')
+    # print(len(data_table))  issue-5
+
+thinkcentre_driver_pack = data_table[4]
+
+
+
+for machine in thinkcentre_driver_pack.find_all('tbody'):
+    rows = machine.find_all('tr')
+    for row in rows:
+        line_output = ''
+        pl_machine = row.find('td').text.replace(',','')
+        line_output = line_output + pl_machine + ','
+        pl_win7 = row.find_all('td')[1].text
+        if (pl_win7 != None):
+            if (pl_win7 != '-'):
+                # print(f'{pl_machine} --> {pl_win7}')
+                line_output = line_output + 'Yes,'
+            else:
+                line_output = line_output + 'No,'
+        pl_win8 = row.find_all('td')[2].text
+        if (pl_win8 != None):
+            if (pl_win8 != '-'):
+                # print(f'{pl_machine} --> {pl_win8}')
+                line_output = line_output + 'Yes,'
+            else:
+                line_output = line_output + 'No,'
+        pl_win10 = row.find_all('td')[3].text
+        if (pl_win10 != None):
+            if (pl_win10 != '-'):
+                line_output = line_output + 'Yes,'
+            else:
+                line_output = line_output + 'No,'
+        if (len(row.find_all('td')) == 4):
+            line_output = line_output + 'Yes'
+        if (len(row.find_all('td')) == 5):
+            try:
+                pl_win11 = row.find_all('td')[4].text
+                if (pl_win11 != None):
+                    if (pl_win11 != '-'):
+                        line_output = line_output + 'Yes'
+                    else:
+                        line_output = line_output + 'No'
+            except:
+                line_output = line_output + 'Yes'
+        with open(output_csv, 'a') as f:
+            f.write(line_output)
+            f.write('\n')
+
+with open(output_csv, "r") as input:
+    with open("temp.txt", "w") as output:
+        # iterate all lines from file
+        for line in input:
+            # if text matches then don't write it
+            if line.strip("\n") != "Sub-series,Yes,Yes,Yes,Yes":
+                output.write(line)
+
+# replace file with original name
+os.replace('temp.txt', output_csv)
