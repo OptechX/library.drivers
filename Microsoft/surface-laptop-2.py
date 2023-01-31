afb828dce3bff6dc703e15745b5683298e5909ec
danijeljw-rpc <danijel@repasscloud.com>
2023-02-01 00:11:30 +1100

    with wait

--- Microsoft/surface-laptop-2.py
+++ Microsoft/surface-laptop-2.py
@@ -43,6 +43,8 @@ except OSError:
 # SETUP CHROME WEBDRIVER (SILENT)
 options = webdriver.ChromeOptions() 
 options.add_argument('--headless')
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 
 # GET DOWNLOAD LINK
