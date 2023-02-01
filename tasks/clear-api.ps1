4b3d3fabda033c29d2186c9b804fa51896549245
danijeljw-rpc <danijel@repasscloud.com>
2023-02-01 23:01:25 +1100

    #10 - remove tasks, tests, python-selenium

--- tasks/clear-api.ps1
+++
@@ -1,5 +0,0 @@
-foreach ($id in (Invoke-RestMethod -Uri https://engine.api.prod.optechx-data.com/v1/DriversCore -Method Get).id)
-{
-    Write-Output "Clearing record: ${id}"
-    Invoke-RestMethod -Uri "https://engine.api.prod.optechx-data.com/v1/DriversCore/${id}" -Method Delete -Headers @{accept="json/application"}
-}
